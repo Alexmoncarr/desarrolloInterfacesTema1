a9ea1454ee762f09d281089238f405222f9d6158
Alejandro <montalvocarrascoalejandro@gmail.com>
2023-09-27 22:39:13 +0200

    dia1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main extends JFrame {
+    public Main (){
+        setTitle("Formulario");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+
+    }
+}
---
+++ src/prueba.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class prueba {
+    private JPanel panel1;
+    private JPanel ParteArriba;
+    private JPanel TituloIzq;
+    private JPanel TituloDere;
+    private JTextField NombreCliente;
+    private JTextArea sddfgfhjhTextArea;
+    private JTextField textField1;
+    private JTextArea sdfgfhjTextArea;
+    private JTextField textField2;
+    private JTextArea dsdfghjhTextArea;
+    private JTextArea sfdgfhjTextArea;
+    private JTextArea asdfghjTextArea;
+    private JTextArea adsfdgfhgfjkTextArea;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+}
